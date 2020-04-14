c88dc6fd622c2a6062681576837153c1d7f19739
root <root@Kali-2019.4>
2020-04-14 16:59:57 -0400

    fixed print_slow

--- TT-helper/print_slow.py
+++ TT-helper/print_slow.py
@@ -1,4 +1,9 @@
-def print_slow(str):
-    for letter in str:
-        print(letter, end='')
-        time.sleep(.1)
+from time import sleep
+
+def print_slow(txt):
+    for x in txt:
+        print(x, end='', flush=True)
+        sleep(0.1)
+    print() # go to new line
+
+print_slow("Slow print function")
